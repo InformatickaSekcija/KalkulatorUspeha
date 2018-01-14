9e79f6e3c5ce21c71caed89252303920c59da1c4
stojan kostic <janstokonza@gmail.com>
2018-01-14 12:30:25 +0100

    Napravio sam funkciju koristeci nizove, ali imam mali problem sa nizovima da ih napravim. Obljasnicu vam kada se budemo videli

---
+++ javascript/kalkulator.js
@@ -0,0 +1,15 @@
+var ZbirV = 0;
+var ZbirM = 0;
+
+function racunanje () {
+for(var i = 0; i < nizV.lenght;i ++)
+{
+    ZbirV += nizV[i];
+}
+
+for(var i = 0; i < nizM.lenght; i++)
+{
+    ZbirM += nizM[i];
+}
+
+}
